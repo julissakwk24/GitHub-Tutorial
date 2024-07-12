b05e5631b5866ac28c174978f760b9ebce0e0538
Julissa Betancourt <julissa.betancourt952@gmail.com>
2024-07-12 14:00:55 -0500

    Initial Commit

---
+++ GitHub Tutorial/GitHub_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_TutorialApp.swift
+//  GitHub Tutorial
+//
+//  Created by Evelyn Betancourt on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
